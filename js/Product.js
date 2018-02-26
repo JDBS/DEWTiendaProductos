1ad4163511204ef36390602c2fa28b14cda96cab
Jaime D <jdbsull@gmail.com>
2018-02-26 19:34:34 +0000

    fix de cdns y prototipo de catalogo

---
+++ js/Product.js
@@ -0,0 +1,17 @@
+class ProductComponent extends React.Component {
+    render(){
+        //Card Columns
+        //https://getbootstrap.com/docs/4.0/components/card/#card-columns
+        return (
+            <div className="card" styles={'width:25px;'}>
+                <img className="card-img-top" src={this.props.data.img} alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.data.name}</h5>
+                    <p className="card-text">{this.props.data.description}</p>
+                    <p className="card-text">{`${this.props.data.price}€`}</p>
+                    <button type="button" className="btn btn-primary">Carrito</button>
+                </div>
+            </div>
+        );
+    }
+}
