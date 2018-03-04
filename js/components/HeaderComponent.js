17341016df6d347b0b20e8d4fa47950396cca550
Yaconeill <yaconeill.amq@gmail.com>
2018-03-04 15:33:51 +0000

    minor fix

--- js/components/HeaderComponent.js
+++ js/components/HeaderComponent.js
@@ -12,22 +12,7 @@ class HeaderComponent extends React.Component{
           <div className=" navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="es/inicio">Inicio <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/es/categorias">Categorías</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/es/contacto">Contacto</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Idiomas
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a className="dropdown-item" href="#">Español</a>
-                  <a className="dropdown-item" href="#">Inglés</a>
-                </div>
+                <a className="nav-link" href="index.html">Inicio <span className="sr-only">(current)</span></a>
               </li>
             </ul>
             <ul className="navbar-nav ml-auto">
