8c985dac84e8a839c546d86a1e0d25aafc6cdd66
Yaconeill <yaconeill.amq@gmail.com>
2018-03-04 12:47:16 +0000

    reajuste del login y arreglo de los iconos

--- js/components/LoginComponent.js
+++ js/components/LoginComponent.js
@@ -5,37 +5,33 @@ class LoginComponent extends React.Component{
   render(){
     return(
       <div>
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="logo my-2 ml-3">
-                <img src="assets/images/eBest.svg" alt=""/>
-            </div>
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link waves-effect waves-light ml-3" href="index.html">Inicio
-                    </a>
-                </li>
-            </ul>
+        <nav className="navbar navbar-light bg-faded">
+          <a className="navbar-brand" href="index.html">
+            <img src="./assets/images/eBest.svg" width="65" height="50" className="d-inline-block align-top" alt="Carrito"/>
+            <h2>Aplicación de Venta</h2>
+          </a>
         </nav>
+        <hr/>
         <br/>
         <div className="container">
           <div className="jumbotron">
-              <h1 className="display-5">Puedes iniciar sesión en
-                  <strong>EBest</strong>
+              <h1 className="display-5 text-center">iniciar sesión en
+                  <strong> EBest</strong>
               </h1>
-              <p className="lead">con su cuenta de redes sociales</p>
+              <p className="lead text-center">Puede entrar con su cuenta de redes sociales</p>
               <hr className="my-4"/>
               <div className="row">
-                <div className="col-12 mx-auto text-center mt-2">
-                  <a className="btn btn-primary btn-lg" href="#" id="google" role="button">
-                      <i className="fa fa-google-plus-square"></i> Sign in with Google</a>
-                  <a className="btn btn-primary btn-lg" href="#" id="facebook" role="button">
-                      <i className="fa fa-facebook-square"></i> Sign in with Facebook</a>
+                <div className="col-12 mx-auto text-center">
+                  <a className="btn btn-primary icon-google btn-lg my-1 mx-1" href="#" id="google" role="button">
+                      Sign in with Google</a>
+                  <a className="btn btn-primary icon-facebook btn-lg my-1 mx-1" href="#" id="facebook" role="button">
+                      Sign in with Facebook</a>
                 </div>
-                <div className="col-12 mx-auto text-center mt-2">
-                  <a className="btn btn-primary btn-lg" href="#" id="github" role="button">
-                      <i className="fa fa-github-square"></i> Sign in with GitHub</a>
-                  <a className="btn btn-primary btn-lg" href="#" id="twitter" role="button">
-                      <i className="fa fa-twitter-square"></i> Sign in with Twitter</a>
+                <div className="col-12 mx-auto text-center">
+                  <a className="btn btn-primary icon-github btn-lg my-1 mx-1" href="#" id="github" role="button">
+                      Sign in with GitHub</a>
+                  <a className="btn btn-primary icon-twitter btn-lg my-1 mx-1" href="#" id="twitter" role="button">
+                      Sign in with Twitter</a>
                 </div>
             </div>
           </div>
